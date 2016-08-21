ac6db26e4c2125d72acfc72d62364c11d6f6c1b9
RWejlgaard <wejlgaard@protonmail.com>
2016-08-21 20:44:50 +0200

    Fixed some powerups not working properly

--- Assets/Scripts/Powerups/DrunkMode.cs
+++ Assets/Scripts/Powerups/DrunkMode.cs
@@ -5,30 +5,30 @@ using UnityStandardAssets.ImageEffects;
 public class DrunkMode : MonoBehaviour {
 
     public float duration = 15;
+    private float countdown;
     private bool active = false;
     private GameObject Camera;
     private Vortex vortex;
     private bool vortexReverse = false;
-    void Start()
-    {
+
+    void Start() {
         Camera = GameObject.FindGameObjectsWithTag("MainCamera")[0];
         vortex = Camera.GetComponent<Vortex>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (duration <= 0)
-        {
+    void Update() {
+        if (countdown <= 0) {
+            active = false;
             vortex.enabled = false;
             vortex.angle = 0;
+            countdown = duration;
         }
-        else if (active)
-        {
-            if (vortex.angle < 80f && !vortexReverse) {
+        else if (active) {
+            if (vortex.angle <= 80f && !vortexReverse) {
                 vortex.angle++;
             }
-            else if (vortex.angle > -80f && vortexReverse) {
+            else if (vortex.angle >= -80f && vortexReverse) {
                 vortex.angle--;
             }
             else if (vortex.angle > 80f && !vortexReverse) {
@@ -37,20 +37,14 @@ public class DrunkMode : MonoBehaviour {
             else if (vortex.angle < -80f && vortexReverse) {
                 vortexReverse = false;
             }
-            duration -= Time.deltaTime;
+            countdown -= Time.deltaTime;
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.gameObject.tag == "Ball")
-        {
-            Camera.GetComponent<Vortex>().enabled = true;
-            active = true;
-        }
-
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag != "Ball") return;
+        Camera.GetComponent<Vortex>().enabled = true;
+        active = true;
         transform.position = new Vector3(1000, 1000, 0);
-
-
     }
 }
--- Assets/Scripts/Powerups/Speed.cs
+++ Assets/Scripts/Powerups/Speed.cs
@@ -4,38 +4,35 @@ using System.Collections;
 public class Speed : MonoBehaviour {
 
     public float duration = 10;
+    private float countdown;
     private bool active = false;
 
     // Use this for initialization
     void Start()
     {
         duration *= 2;
+        countdown = duration;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (duration <= 0)
+        if (countdown <= 0)
         {
             Time.timeScale = 1;
-            
+            countdown = duration;
         }
         else if (active)
         {
-            duration -= Time.deltaTime;
+            countdown -= Time.deltaTime;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Ball")
-        {
-            Time.timeScale = 2f;
-            active = true;
-        }
-
+        if (col.gameObject.tag != "Ball") return;
+        Time.timeScale = 2f;
+        active = true;
         transform.position = new Vector3(1000, 1000, 0);
-
-
     }
 }
